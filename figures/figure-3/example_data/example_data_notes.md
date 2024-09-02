54128fe1469b3a44fce4d705a03b4b42290e09ff
Zach Hensel <zhensel@gmail.com>
2024-09-02 15:49:49 +0100

    update repo for revision submission

---
+++ figures/figure-3/example_data/example_data_notes.md
@@ -0,0 +1,25 @@
+# Example data for FtsB-PerM figure
+
+## Plasmid loss
+
+By chance, one field of view imaging pJRF002 and pJRF007 in standard (1 nM aTc, 100 uM IPTG) conditions showed that one microcolony had lost pJRF007. This happens occasionally given lack of antibiotics in gel pads used for the last step of growth and imaging.
+
+Scale bar, 5 um
+
+## PerM construct comparisons
+
+All 37C, EZ-rich media, 100 uM IPTG
+
+We observed weak expression of mNeonGreen-PerM with some indication of membrane localizationa nd very low expression of PerM-mNeonGreen. Meanwhile, the PerMn1-mNeonGreen constructed showed much higher fluorescence, albeit without strong membrane localization. By comparison, Pf3-mNeonGreen clearly exhibits strong membrane localization. We suspected that the difference is due to PerM instability and subsequent release of a degradation product ending in mNeonGreen into the cytoplasm.
+
+pJRF001, pJRF004, pJRF004n1, pJRF010 left to right
+
+Scal bar, 5 um. All mNeonGreen images at same brightness/contrast.
+
+## FtsB construct comparison
+
+All 37C, 10 nM aTc, EZ Rich
+
+pJRF002, pJRF007, pJRF007dH left to right
+
+5 um scale bar, all mTq2 images same brightness/contrast
---
+++ notebooks/fiji-trackmate PALM analysis/trackmate_mneongreen.py
@@ -0,0 +1,133 @@
+# Process mNeonGreen movies as with mEos3.2 movies but cutting out first 250 frames to remove initial photobleaching
+
+import sys
+import os
+import glob
+from java.io import File
+
+from ij import IJ
+from ij import WindowManager
+
+from fiji.plugin.trackmate import Model
+from fiji.plugin.trackmate import Settings
+from fiji.plugin.trackmate import TrackMate
+from fiji.plugin.trackmate import SelectionModel
+from fiji.plugin.trackmate import Logger
+from fiji.plugin.trackmate.detection import DogDetectorFactory
+from fiji.plugin.trackmate.tracking.jaqaman import SimpleSparseLAPTrackerFactory
+from fiji.plugin.trackmate.gui.displaysettings import DisplaySettingsIO
+import fiji.plugin.trackmate.visualization.hyperstack.HyperStackDisplayer as HyperStackDisplayer
+import fiji.plugin.trackmate.features.FeatureFilter as FeatureFilter
+from fiji.plugin.trackmate.io import TmXmlWriter
+from fiji.plugin.trackmate.util import TMUtils
+from fiji.plugin.trackmate.visualization.table import TrackTableView
+from fiji.plugin.trackmate.visualization.table import AllSpotsTableView
+from fiji.plugin.trackmate import SelectionModel
+from fiji.plugin.trackmate.gui.displaysettings import DisplaySettings
+import fiji.plugin.trackmate.action.ExportTracksToXML as ExportTracksToXML
+
+# We have to do the following to avoid errors with UTF8 chars generated in 
+# TrackMate that will mess with our Fiji Jython.
+reload(sys)
+sys.setdefaultencoding('utf-8')
+ 
+# Get a directory
+#@ File (label="Select a directory", style="directory") dir
+
+def process_directory(image_directory):
+
+	# Get list of image files in directory
+	files = glob.glob(image_directory + '514_mNG_stream*.tif')
+	
+	i = 0
+	N = len(files)
+	
+	# Process and save trackmate analysis for each PALM movie
+	for f in files:
+	
+		print(' '.join(['Processing file',str(i+1),'of',str(N)]))
+		i += 1
+	
+		imp = IJ.openImage(f)
+		imp.show()
+		
+		# TODO Add code here to remove first 250 frames !!
+		
+		# 100 nm pixels and 33 ms frame rate
+		imp.getCalibration().setXUnit("nm");
+		
+		IJ.run(imp, "Slice Remover", "first=1 last=250 increment=1");
+		IJ.run(imp, "Properties...", "channels=1 slices=1 frames=250 pixel_width=100 pixel_height=100 voxel_depth=100 frame=[33 ms]");
+		
+		model = Model()
+		model.setLogger(Logger.IJ_LOGGER)
+		
+		settings = Settings(imp)
+		 
+		# TODO FIND GOOD PARAMETERS FOR mNG
+		# Configure detector - We use the Strings for the keys
+		settings.detectorFactory = DogDetectorFactory()
+		settings.trackerSettings = settings.detectorFactory.getDefaultSettings()
+		settings.detectorSettings = {
+		    'DO_SUBPIXEL_LOCALIZATION' : True,
+		    'RADIUS' : 2.5, #250.,
+		    'TARGET_CHANNEL' : 1,
+		    'THRESHOLD' : 200.,
+		    'DO_MEDIAN_FILTERING' : True,
+		}
+		
+		settings.trackerFactory = SimpleSparseLAPTrackerFactory()
+		settings.trackerSettings = settings.trackerFactory.getDefaultSettings() # almost good enough
+		settings.trackerSettings['GAP_CLOSING_MAX_DISTANCE'] = 500.
+		settings.trackerSettings['LINKING_MAX_DISTANCE'] = 500.
+		settings.trackerSettings['MAX_FRAME_GAP'] = 0
+		
+		settings.addAllAnalyzers()
+		
+		trackmate = TrackMate(model, settings)
+		
+		ok = trackmate.checkInput()
+		if not ok:
+		    sys.exit(str(trackmate.getErrorMessage()))
+		 
+		ok = trackmate.process()
+		if not ok:
+		    sys.exit(str(trackmate.getErrorMessage()))
+		    
+		selectionModel = SelectionModel( model )
+		
+		ds = DisplaySettingsIO.readUserDefault()
+		 
+		displayer =  HyperStackDisplayer( model, selectionModel, imp, ds )
+		displayer.render()
+		displayer.refresh()
+		
+		model.getLogger().log( str( model ) )
+		
+		# save trackmate file with default name in data folder
+		saveFile = TMUtils.proposeTrackMateSaveFile( settings, Logger.IJ_LOGGER )
+		writer = TmXmlWriter( saveFile, Logger.IJ_LOGGER )
+		writer.appendLog( Logger.IJ_LOGGER.toString() )
+		writer.appendModel( trackmate.getModel() )
+		writer.appendSettings( trackmate.getSettings() )
+		writer.writeToFile();
+		
+		# Export tracks only XML
+		saveFile = File(f[:-4] + '_tracks.xml')
+		ExportTracksToXML.export(trackmate.getModel(), trackmate.getSettings(), saveFile)
+	
+		imp.close()
+		
+	return 'Done!'
+
+d = dir.getPath()
+subdirectories = [os.path.join(d, o) for o in os.listdir(d) 
+                    if os.path.isdir(os.path.join(d,o))]
+
+for s in subdirectories:
+	if s.split('/')[-1][1]=='_':
+		print('Processing ' + s)
+		# print(process_directory(s + '/')) # for 231004 before directories were renamed to be consistent
+		print(process_directory(s + '/mNeonGreen_514/')) # for other days
+
+
